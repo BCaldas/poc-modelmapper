bb40eda391b8507eca4213816d029abfa3894289
Bruno Caldas <brunopaivacaldas@hotmail.com>
2019-05-24 20:33:48 -0300

    - Inclusão de comentários para auxílio do entendimento.

--- src/main/java/com/example/modelmapper/config/MapperConfig.java
+++ src/main/java/com/example/modelmapper/config/MapperConfig.java
@@ -10,10 +10,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MapperConfig {
 
+    //Criamos um bean para poder injeta-lo via @Autowired
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
 
+        //O Model mapper consegue mapear as propriedades da entidade/dto automaticamente
+        //através dos nomes dos get/set. Quando o nome das propriedades q estão sendo mapeada é diferentes
+        //podemos utilizar um PropertyMap para dizer ao ModelMapper como mapear as propriedades.
         PropertyMap<Apolice, ApoliceDTO> apoliceToPoliceDtoMap;
         apoliceToPoliceDtoMap = new PropertyMap<Apolice, ApoliceDTO>() {
             protected void configure() {
